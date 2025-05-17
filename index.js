fad2f15314bf5862c7c5461ae86e8e830f3621bb
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2025-05-17 11:45:25 +0530

    1

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express=require('express');
-
+const  mongoose=require('mongoose');
+const  bodyParser=require('body-parser');
+require('dotenv').config();
 const pos=express();
-pos.listen(3000,()=>{
-    console.log('server running');
-});
+const  SERVER_PORT=process.env.SERVER_PORT || 3000;
+
+pos.use(bodyParser.urlencoded())
+pos.use(bodyParser.json())
+
+try{
+    mongoose.connect(`${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+    pos.listen(SERVER_PORT,()=>{
+        console.log(`server running on port ${SERVER_PORT}`);
+    });
+
+}catch (e) {
+    console.log(e);
+}
 
 pos.get('/test-api',(req,resp)=>{
-    resp.json({'message':'Hi server is running'});
-})
+    return resp.json({'message':'Hi server is running...'});
+});
+
+pos.post('/create',(req,resp)=>{
+    console.log( req.body);
+    return  resp.json({'data':req.body});
+});
